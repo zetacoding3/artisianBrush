fc6ea48cf0ed6eb1360a9d2cf5aa8c37a7ea2a0d
zetacoding3 <zetacoding.academics@gmail.com>
2025-05-10 09:42:25 +0530

    initial commit

---
+++ ArtisanBrush/frontend/src/pages/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function About() {
+    return (
+        <div>
+            <div className="container-fluid g-0 px-3 py-5">
+                <img src="/img/about.avif" alt="" className="img-fluid col-md-4 float-start mx-4" />
+                <h1 className="text-center fw-semibold">Our Story</h1>
+                <p className='fs-5'>
+                    ArtisanBrush is a celebration of creativity, tradition, and soul. We are a boutique studio that specializes in handmade paintings, crafted with passion and precision by skilled artists. Each piece we create tells a story—rich in color, emotion, and timeless beauty.                </p>
+                <p className='fs-5'>
+                    Our collection spans across styles—from contemporary abstracts to cultural motifs, from minimalist brushwork to vivid landscapes. Whether you're looking to add warmth to your home, soul to your office, or a unique gift for a loved one, ArtisanBrush brings art that resonates.                </p>
+                <p className='fs-5'>
+                    We believe that true art comes from the heart, not machines. That’s why every painting is 100% handcrafted, using premium-quality materials and sustainable practices to ensure long-lasting impact—visually and emotionally.                </p>
+                <p className='fs-5'>
+                    At ArtisanBrush, we don't just sell art. We create experiences that connect people through paint.
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default About
